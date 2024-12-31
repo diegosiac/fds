e3de7f1634b4166b1b235743ed83e04d4d924409
diegosiac <diego35502@hotmail.com>
2024-12-30 18:41:51 -0600

    refactor prompts and translate logic for improved readability and consistency

--- src/translate.ts
+++ src/translate.ts
@@ -1,111 +1,111 @@
-import { openai } from '@ai-sdk/openai'
-import { generateText } from 'ai'
-import fs from 'node:fs'
-import type { Term } from './types/term'
-import { initialPath, translationsPath } from './constants/file-paths'
-import { systemContextTranslatePrompt } from './constants/prompts'
+import { openai } from "@ai-sdk/openai";
+import { generateText } from "ai";
+import fs from "node:fs";
+import type { Term } from "./types/term";
+import { initialPath, translationsPath } from "./constants/file-paths";
+import { systemContextTranslatePrompt } from "./constants/prompts";
 
-const inputPath = initialPath
-const outputPath = translationsPath
+const inputPath = initialPath;
+const outputPath = translationsPath;
 
-let totalTokens = 0
-let promptTokens = 0
-let completionTokens = 0
+let totalTokens = 736;
+let promptTokens = 651;
+let completionTokens = 85;
 
-const maxBatchSize: null | number = null
+const maxBatchSize: null | number = null;
 
 const traduceAI = async (question: string) => {
-	const { text, usage } = await generateText({
-		model: openai('gpt-4o'),
-		prompt: question,
-		system: systemContextTranslatePrompt,
-		maxTokens: 1500,
-		temperature: 0.4,
-	})
+  const { text, usage } = await generateText({
+    model: openai("gpt-4o"),
+    prompt: question,
+    system: systemContextTranslatePrompt,
+    maxTokens: 1500,
+    temperature: 0.4,
+  });
 
-	totalTokens += usage.totalTokens
-	promptTokens += usage.promptTokens
-	completionTokens += usage.completionTokens
+  totalTokens += usage.totalTokens;
+  promptTokens += usage.promptTokens;
+  completionTokens += usage.completionTokens;
 
-	return text
-}
+  return text;
+};
 
 const logTokens = () => {
-	console.log(`Tokens totales: ${totalTokens}`)
-	console.log(`Tokens de la pregunta: ${promptTokens}`)
-	console.log(`Tokens de la respuesta: ${completionTokens}`)
-}
+  console.log(`Tokens totales: ${totalTokens}`);
+  console.log(`Tokens de la pregunta: ${promptTokens}`);
+  console.log(`Tokens de la respuesta: ${completionTokens}`);
+};
 
 const readProgress = (): Term[] => {
-	if (fs.existsSync(outputPath)) {
-		const data = fs.readFileSync(outputPath, 'utf8')
+  if (fs.existsSync(outputPath)) {
+    const data = fs.readFileSync(outputPath, "utf8");
 
-		return JSON.parse(data)
-	}
+    return JSON.parse(data);
+  }
 
-	return []
-}
+  return [];
+};
 
 const saveProgress = (translatedQuestions: Term[]) => {
-	logTokens()
+  logTokens();
 
-	fs.writeFileSync(outputPath, JSON.stringify(translatedQuestions, null, 2))
-}
+  fs.writeFileSync(outputPath, JSON.stringify(translatedQuestions, null, 2));
+};
 
 const readQuestions = (): Term[] => {
-	const data = fs.readFileSync(inputPath, 'utf8')
+  const data = fs.readFileSync(inputPath, "utf8");
 
-	return JSON.parse(data)
-}
+  return JSON.parse(data);
+};
 
-;(async () => {
-	const questions = readQuestions()
-	let translatedQuestions = readProgress()
-	let startIndex = translatedQuestions.length
+(async () => {
+  const questions = readQuestions();
+  let translatedQuestions = readProgress();
+  let startIndex = translatedQuestions.length;
 
-	console.log(`Comenzando desde el índice: ${startIndex}`)
+  console.log(`Comenzando desde el índice: ${startIndex}`);
 
-	let completedTask = 0
+  let completedTask = 0;
 
-	for (let i = startIndex; i < questions.length; i++) {
-		try {
-			completedTask++
+  for (let i = startIndex; i < questions.length; i++) {
+    try {
+      completedTask++;
 
-			console.log(
-				`Traduciendo pregunta ${i + 1} de ${
-					maxBatchSize ? startIndex + maxBatchSize : questions.length
-				}...`
-			)
+      console.log(
+        `Traduciendo pregunta ${i + 1} de ${
+          maxBatchSize ? startIndex + maxBatchSize : questions.length
+        }...`,
+      );
 
-			const translatedQuestion = await traduceAI(questions[i].question)
+      const translatedQuestion = await traduceAI(questions[i].question);
 
-			translatedQuestions.push({
-				question: translatedQuestion,
-				answer: questions[i].answer,
-			})
+      translatedQuestions.push({
+        question: translatedQuestion,
+        answer: questions[i].answer,
+      });
 
-			if (!maxBatchSize) continue
+      if (!maxBatchSize) continue;
 
-			if (completedTask >= maxBatchSize) {
-				saveProgress(translatedQuestions)
+      if (completedTask >= maxBatchSize) {
+        saveProgress(translatedQuestions);
 
-				console.log(`Se han completado ${completedTask} tareas.`)
+        console.log(`Se han completado ${completedTask} tareas.`);
 
-				return
-			}
-		} catch (error) {
-			console.log(error)
+        return;
+      }
+    } catch (error) {
+      console.log(error);
 
-			console.error(
-				`Fallo en la traducción en el índice ${i}. Guardando progreso...`
-			)
+      console.error(
+        `Fallo en la traducción en el índice ${i}. Guardando progreso...`,
+      );
 
-			saveProgress(translatedQuestions)
-			return
-		}
-	}
+      saveProgress(translatedQuestions);
+      return;
+    }
+  }
 
-	saveProgress(translatedQuestions)
+  saveProgress(translatedQuestions);
 
-	console.log('Todas las preguntas han sido traducidas con éxito.')
-})()
+  console.log("Todas las preguntas han sido traducidas con éxito.");
+})();
